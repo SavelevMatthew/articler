50e996d51763c8e6597ee3ac2b8cb82cf2fd06a5
SavelevMatthew <savelevmatthew@gmail.com>
2025-01-19 19:45:15 +0500

    ci: finalize eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,28 @@
-import globals from 'globals'
+import { createRequire } from 'node:module'
+import tsEslint from 'typescript-eslint'
 import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import jestPlugin from 'eslint-plugin-jest'
 import importPlugin from 'eslint-plugin-import'
 
-/** @type {import('eslint').Linter.Config[]} */
-export default tseslint.config(
-    { files: ['**/*.{js,mjs,cjs,ts}'] },
+const require = createRequire(import.meta.url)
+const jestVersion = require('jest/package.json').version
+
+/** @type {import('eslint').Linter.Config}  */
+const config = tsEslint.config(
+    {
+        ignores: ['**/dist', '**/node_modules', '**/.yarn'],
+    },
+    eslint.configs.recommended,
+    tsEslint.configs.recommendedTypeChecked,
+    tsEslint.configs.stylisticTypeChecked,
     {
         languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.jest,
-            },
-
             parserOptions: {
-                project: ['tsconfig.eslint.json'],
                 projectService: true,
-                tsconfigRootDir: '.',
+                tsconfigRootDir: import.meta.dirname,
             },
         },
     },
-    eslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
     importPlugin.flatConfigs.recommended,
     {
         rules: {
@@ -41,8 +42,56 @@ export default tseslint.config(
             },
         },
     },
+    jestPlugin.configs['flat/recommended'],
+    {
+        files: ['**/*.spec.ts', '**/*.test.ts'],
+        plugins: { jest: jestPlugin },
+        languageOptions: {
+            globals: jestPlugin.environments.globals.globals,
+        },
+        rules: {
+            'jest/no-disabled-tests': 'error',
+            'jest/no-focused-tests': 'error',
+            'jest/no-identical-title': 'error',
+            'jest/prefer-to-have-length': 'warn',
+            'jest/valid-expect': 'error',
+            'jest/no-conditional-expect': 'warn',
+        },
+        settings: {
+            jest: {
+                version: jestVersion,
+            },
+        },
+    },
+    {
+        rules: {
+            'require-await': 'off',
+            '@typescript-eslint/require-await': 'off',
+            '@typescript-eslint/unbound-method': 'off',
+            '@typescript-eslint/consistent-type-definitions': 'off',
+            '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-unsafe-return': 'off',
+            '@typescript-eslint/no-unsafe-argument': 'off',
+            '@typescript-eslint/no-unsafe-member-access': 'off',
+            '@typescript-eslint/restrict-template-expressions': 'off',
+            '@typescript-eslint/prefer-nullish-coalescing': 'off',
+            '@typescript-eslint/array-type': ['error', { default: 'generic', readonly: 'generic' }],
+            '@typescript-eslint/consistent-type-assertions': [
+                'error',
+                { assertionStyle: 'as', objectLiteralTypeAssertions: 'never' },
+            ],
+            '@typescript-eslint/consistent-type-imports': [
+                'error',
+                { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
+            ],
+            '@typescript-eslint/consistent-type-exports': 'error',
+            '@typescript-eslint/no-confusing-non-null-assertion': 'error',
+        },
+    },
     {
         files: ['**/*.mjs', '**/*.js'],
-        extends: [tseslint.configs.disableTypeChecked],
+        extends: [tsEslint.configs.disableTypeChecked],
     },
 )
+
+export default config
